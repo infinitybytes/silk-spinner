932fd96c514fc9a65d9ca600749ed9ce3ea38b94
akinder <5808558+infinitybytes@users.noreply.github.com>
2021-08-20 19:19:50 -0400

    user management

--- src/main/java/ai/ibytes/ingester/controllers/Admin.java
+++ src/main/java/ai/ibytes/ingester/controllers/Admin.java
@@ -1,10 +1,14 @@
 package ai.ibytes.ingester.controllers;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +27,28 @@ public class Admin {
         model.put("user",(user!=null) ? user.getName() : "ANON");
 
         return new ModelAndView("acp-users", model);
+    }
+
+    @PostMapping(path="/acp-users.html")
+    public ModelAndView createNewUser(Principal user, Map<String, Object> model, @RequestParam("username") String username, @RequestParam("pass1") String password)    {
+        List<String> errors = new ArrayList<>();
+        List<String> msgs = new ArrayList<>();
+
+        if(username.isEmpty() || password.isEmpty())    {
+            errors.add("Username and password cannot be blank.");
+        }
+
+        if(errors.size()>0) {
+            model.put("errors", errors);
+        }
+
+        if(errors.size()==0)    {
+            // Create user
+
+            // Save to disk
+        }
+
+        return getUserPage(user, model);
     }
 
     @GetMapping( path = "/acp-xp.html")
