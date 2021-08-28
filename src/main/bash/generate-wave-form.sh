19b002530081a0dc98318a7d646c57c91d89e308
akinder <5808558+infinitybytes@users.noreply.github.com>
2021-08-28 15:05:28 -0400

    shell versions

--- src/main/bash/generate-wave-form.sh
+++ src/main/bash/generate-wave-form.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-ffmpeg -i $1 -filter_complex \
-"[0:a]aformat=channel_layouts=mono, \
- compand, \
- showwavespic=draw=full:filter=peak:s=2048x800,showinfo[fg]; \
- color=s=2048x800:color=#44582c, \
- drawgrid=width=iw/10:height=ih/5:color=#9cf42f@0.1[bg]; \
- [bg][fg]overlay=format=auto,showinfo,drawbox=x=(iw-w)/2:y=(ih-h)/2:w=iw:h=1:color=#9cf42f" \
--frames:v 1 $1.png
+ffmpeg -i $1 -filter_complex "[0:a]aformat=channel_layouts=mono, showwavespic=draw=full:filter=peak:s=128x128,showinfo[fg]; color=s=128x128:color=#ffffff, drawgrid=width=iw/10:height=ih/5:color=#3366ff@0.1[bg]; [bg][fg]overlay=format=auto,showinfo,drawbox=x=(iw-w)/2:y=(ih-h)/2:w=iw:h=1:color=#3366ff" -frames:v 1 $1.png
