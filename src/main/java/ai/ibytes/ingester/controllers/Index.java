f7d4cc3e07de7e6f5f6bc995c7b8270ba4a430cc
akinder <5808558+infinitybytes@users.noreply.github.com>
2022-01-18 13:36:18 -0500

    refactor listing page

--- src/main/java/ai/ibytes/ingester/controllers/Index.java
+++ src/main/java/ai/ibytes/ingester/controllers/Index.java
@@ -2,12 +2,14 @@ package ai.ibytes.ingester.controllers;
 
 import java.security.Principal;
 import java.util.Map;
+import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ai.ibytes.ingester.config.StorageConfig;
@@ -31,12 +33,12 @@ public class Index {
     private ObjectMapper objectMapper = new ObjectMapper();
     
     @GetMapping( path = {"/", "/index.html"})
-    public ModelAndView getIndexPage(Principal user, Map<String, Object> model)   {
+    public ModelAndView getIndexPage(Principal user, Map<String, Object> model, @RequestParam("id") Optional<String> id)   {
         model.put("user",(user!=null) ? user.getName() : "ANON");
 
         try {
             ftpClient.connect();
-            model.put("ftpfiles", ftpClient.ls());
+            model.put("ftpfiles", (id.isPresent()) ? ftpClient.ls(id.get()) : ftpClient.ls());
             ftpClient.disconnect();
         } catch (Exception e) {
             log.error("Error listing remote dir",e);
