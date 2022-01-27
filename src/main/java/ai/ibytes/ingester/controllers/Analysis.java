eb4584d16282f5d1b5cd8189c860717cef1d397d
akinder <5808558+infinitybytes@users.noreply.github.com>
2022-01-26 21:50:53 -0500

    Analyze from a season level

--- src/main/java/ai/ibytes/ingester/controllers/Analysis.java
+++ src/main/java/ai/ibytes/ingester/controllers/Analysis.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 import org.apache.commons.net.ftp.FTPFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,32 +40,45 @@ public class Analysis {
     public ModelAndView getIndexPage(Principal user, Map<String, Object> model, @RequestParam("id") Optional<String> id) throws IOException   {
         model.put("user",(user!=null) ? user.getName() : "ANON");
 
+        // Run analysis
+        analyze(id.get());
+
+        model.put("message", "Analysis complete.");
+        return new ModelAndView("analysis-start", model);
+    }
+
+    private void analyze(String directory)   {
         ftpClient.connect();
         try {
-            List<FTPFile> datafiles = ftpClient.ls(id.get());
+            List<FTPFile> datafiles = ftpClient.ls(directory);
 
             // Submit the analyzer to a threadpool
             datafiles.stream().forEach(file -> {
                 // skip goback
                 if(!file.getName().endsWith(".")) {
-                    // recurse
-                    File localFile = new File(storageConfig.getTempStore(), file.getName());
-                    if(!localFile.exists())  {
-                        ftpClient.getRemote(id.get() + '/' + file.getName(), localFile);
+                    // directory? recurse
+                    if(file.isDirectory())  {
+                        analyze(directory + '/' + file.getName());
+                    }
+                    else    {
+                        File localFile = new File(storageConfig.getTempStore(), file.getName());
+                        if(!localFile.exists())  {
+                            ftpClient.getRemote(directory + '/' + file.getName(), localFile);
 
-                        // submit to the analyzer
-                        DataFile df = new DataFile();
-                        df.setDirName(id.get());
-                        df.setName(localFile.getName());
-                        df.setLocalTempFile(localFile.toPath());
+                            // submit to the analyzer
+                            DataFile df = new DataFile();
+                            df.setDirName(directory);
+                            df.setName(localFile.getName());
+                            df.setLocalTempFile(localFile.toPath());
 
-                        analyzeAudio.setDataFile(df);
+                            analyzeAudio.setDataFile(df);
 
-                        // create json data file locally
-                        storageService.store(id.get(),file.getName());
+                            // create json data file locally
+                            storageService.store(directory,file.getName());
 
-                        // send to analyzer
-                        analyzeAudio.run();
+                            // send to analyzer
+                            analyzeAudio.run();
+                        }
                     }
                 }
             });
@@ -74,7 +86,5 @@ public class Analysis {
             log.error("Error listing remote dir",e);
         } 
         ftpClient.disconnect();
-
-        return new ModelAndView("index", model);
     }
 }
