76fbc81976b3e35b2c2e84c6deca34b500ae5bab
Adam Kinder <ak@ibytes.ai>
2022-05-14 01:20:19 -0400

    waveform

--- src/main/java/ai/ibytes/ingester/controllers/AnalysisController.java
+++ src/main/java/ai/ibytes/ingester/controllers/AnalysisController.java
@@ -11,19 +11,27 @@ import org.springframework.web.servlet.ModelAndView;
 import ai.ibytes.ingester.storage.FileSystemStorageService;
 import ai.ibytes.ingester.storage.model.DataFile;
 import ai.ibytes.ingester.storage.model.Site;
+import ai.ibytes.ingester.tasks.GenerateWaveForm;
 
 @Controller
 public class AnalysisController {
     @Autowired
     private FileSystemStorageService storageService;
 
+    @Autowired
+    private GenerateWaveForm waveFormTask;
+
     @GetMapping( path = "/analyze-file.html")
     public ModelAndView getAnalyzeFile(Map<String, Object> model, @RequestParam("siteId") String siteId, @RequestParam("id") String id)   {
         // Get the datafile
         Site site = storageService.getSite(siteId);
         model.put("site",site);
-        
+
         DataFile dataFile = storageService.getDataFile(siteId, id);
+
+        // Generate a waveform
+        waveFormTask.generate(dataFile);
+
         model.put("dataFile",dataFile);
 
         return new ModelAndView("datafile", model);
