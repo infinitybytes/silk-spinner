d80e5a733799dca742fcb7632e188fb7c4fc2bc0
Adam Kinder <ak@ibytes.ai>
2022-05-20 18:24:23 -0400

    file view

--- src/main/java/ai/ibytes/ingester/controllers/DatafileController.java
+++ src/main/java/ai/ibytes/ingester/controllers/DatafileController.java
@@ -1,6 +1,7 @@
 package ai.ibytes.ingester.controllers;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,8 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import ai.ibytes.ingester.storage.FileSystemStorageService;
+import ai.ibytes.ingester.storage.model.DataFile;
+import ai.ibytes.ingester.storage.model.Site;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -23,6 +27,20 @@ public class DatafileController {
     @Autowired
     private FileSystemStorageService storageService;
 
+    @GetMapping( path = "/file.html")
+    public ModelAndView getFile(Map<String, Object> model, @RequestParam("siteId") String siteId, @RequestParam("id") String id)   {
+        // Get the datafile
+        Site site = storageService.getSite(siteId);
+        model.put("site",site);
+
+        DataFile dataFile = storageService.getDataFile(siteId, id);
+
+        // Send to model
+        model.put("dataFile",dataFile);
+
+        return new ModelAndView("datafile", model);
+    }
+    
     @GetMapping("/datafile-bytes.html")
     @ResponseBody FileSystemResource getFileBytes(@RequestParam("siteId") String siteId, @RequestParam("id") String id, HttpServletResponse response)   {
         // Load the actual file
